482f32cd1b2dd07bd16c51225a091cfbfd083ad5
juan.lage@gmail.com <juan.lage@gmail.com>
2018-01-26 21:57:16 +0100

    second demo almost ready

---
+++ cleur-2018-useg/newapp.php
@@ -0,0 +1,33 @@
+  <?php
+    $servername = "10.50.50.200";
+    $username = "app-user";
+    $password = "cisco";
+    $db_name = "test";
+    $db_port = "3306";
+
+    // create connection to the DB
+    $conn = mysqli_connect($servername, $username, $password, $db_name);
+
+    // check that connection worked
+    if (!$conn) {
+       die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected succesfully to the DB<br>";
+
+    $string = '<br>This is an awesome App!<br>';
+
+    // Print text
+    echo $string;
+
+    // print hostname
+    echo "<br>Running from  ";
+    echo gethostname();
+
+   //  show image
+   $file = 'giphy.gif';
+   $imginfo = getimagesize($file);
+   header("Content-type: {$imginfo['mime']}");
+   echo file_get_contents($file);
+   //readfile($file);
+   exit();
+   ?>
